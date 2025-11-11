5e11c8be4ef529e2841e6bca64a9cca87c7689db
roong-ji <mhienegjyuu@naver.com>
2025-11-11 13:42:56 +0900

    feat: 배경 스크롤링

--- Assets/02.Scripts/Enviroment/BackgroundScroll.cs
+++ Assets/02.Scripts/Enviroment/BackgroundScroll.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class BackgroundScroll : MonoBehaviour
 {
-    // 배경 스크롤
+    [Header("배경 스크롤 설정")]
+    [SerializeField] private Material _backgroundMaterial;
+    [SerializeField] private float _scrollSpeed;
 
-    // - 머터리얼
-    // - 스크롤 속도
+    private void Awake()
+    {
+        _backgroundMaterial.mainTextureOffset = Vector2.zero;
+    }
 
     private void Update()
     {
         // 방향을 구한다.
+        Vector2 direction = Vector2.up;
         // 움직인다.
+        _backgroundMaterial.mainTextureOffset += direction * _scrollSpeed * Time.deltaTime;
     }
 }
