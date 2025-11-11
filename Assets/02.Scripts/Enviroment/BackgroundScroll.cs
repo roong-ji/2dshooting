e901056a1c430b33aa968aefb440f90fbdccb009
roong-ji <mhienegjyuu@naver.com>
2025-11-11 13:15:16 +0900

    feat: 배경 머터리얼 설정

---
+++ Assets/02.Scripts/Enviroment/BackgroundScroll.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    // 배경 스크롤
+
+    // - 머터리얼
+    // - 스크롤 속도
+
+    private void Update()
+    {
+        // 방향을 구한다.
+        // 움직인다.
+    }
+}
