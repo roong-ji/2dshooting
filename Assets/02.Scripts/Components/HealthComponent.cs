a18de5cffa583d5ba62f3375fb4a656b182d896a
roong-ji <mhienegjyuu@naver.com>
2025-11-11 14:24:01 +0900

    feat: enemy 폭파 이펙트 적용

--- Assets/02.Scripts/Components/HealthComponent.cs
+++ Assets/02.Scripts/Components/HealthComponent.cs
@@ -7,13 +7,27 @@ public class HealthComponent : MonoBehaviour
     [Header("체력")]
     [SerializeField] private float _health;
 
+    [Header("폭팔 프리팹")]
+    [SerializeField] private GameObject _explosionPrefab;
+
     public void TakeDamage(float damage)
     {
         _health -= damage;
 
         if (_health > 0f) return;
         Destroy(gameObject);
+        MakeExploisionEffect();
+        DropItem();
+    }
 
+    private void MakeExploisionEffect()
+    {
+        if (_explosionPrefab == null) return;
+        Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+    }
+
+    private void DropItem()
+    {
         if (_itemDropper == null) return;
         _itemDropper.DropItem(transform.position);
     }
--- Assets/02.Scripts/Enviroment/BackgroundScroll.cs
+++ Assets/02.Scripts/Enviroment/BackgroundScroll.cs
@@ -5,6 +5,7 @@ public class BackgroundScroll : MonoBehaviour
     [Header("배경 스크롤 속도 설정")]
     [SerializeField] private float _scrollSpeed;
     private Material _backgroundMaterial;
+    private Vector2 direction = Vector2.up;
 
     private void Awake()
     {
@@ -13,9 +14,6 @@ public class BackgroundScroll : MonoBehaviour
 
     private void Update()
     {
-        // 방향을 구한다.
-        Vector2 direction = Vector2.up;
-        // 움직인다.
         _backgroundMaterial.mainTextureOffset += direction * _scrollSpeed * Time.deltaTime;
     }
 }
