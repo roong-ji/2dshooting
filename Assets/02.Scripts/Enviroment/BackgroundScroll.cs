415196d1b4f1becfbd9e5a2f68dd9bb372018aa3
roong-ji <mhienegjyuu@naver.com>
2025-11-12 09:29:20 +0900

    fix: Material Property Block 적용

--- Assets/02.Scripts/Enviroment/BackgroundScroll.cs
+++ Assets/02.Scripts/Enviroment/BackgroundScroll.cs
@@ -4,19 +4,31 @@ public class BackgroundScroll : MonoBehaviour
 {
     [Header("배경 스크롤 속도 설정")]
     [SerializeField] private float _scrollSpeed;
+
+    private SpriteRenderer _spriteRenderer;
     private Material _backgroundMaterial;
-    //private MaterialPropertyBlock _backgroundMpb;
-    private Vector2 direction = Vector2.up;
+    private MaterialPropertyBlock _backgroundMpb;
+
+    //private Vector2 direction = Vector2.up;
+    private Vector4 offsetVector = Vector4.zero;
 
     private void Awake()
     {
-        _backgroundMaterial = GetComponent<SpriteRenderer>().material;
-        //_backgroundMpb = new MaterialPropertyBlock();
-        //GetComponent<SpriteRenderer>().GetPropertyBlock(_backgroundMpb);
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _backgroundMaterial = _spriteRenderer.material;
+        _backgroundMpb = new MaterialPropertyBlock();
+        _spriteRenderer.GetPropertyBlock(_backgroundMpb);
+        offsetVector.x = 1;
+        offsetVector.y = 1;
     }
 
     private void Update()
     {
-        _backgroundMaterial.mainTextureOffset += direction * _scrollSpeed * Time.deltaTime;
+        offsetVector.w += _scrollSpeed * Time.deltaTime;
+        _backgroundMpb.SetVector("_MainTex_ST", offsetVector);
+
+        _spriteRenderer.SetPropertyBlock(_backgroundMpb);
+
+        //_backgroundMaterial.mainTextureOffset += direction * _scrollSpeed * Time.deltaTime;
     }
 }
