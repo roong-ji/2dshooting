8f7ceba82658daf379e5729f1f8c1abd1ebbf6ba
roong-ji <mhienegjyuu@naver.com>
2025-11-17 09:05:28 +0900

    feat: 아이콘 추가

--- Assets/02.Scripts/Manager/DamageManager.cs
+++ Assets/02.Scripts/Manager/DamageManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageManager : MonoBehaviour
 {
@@ -21,15 +22,32 @@ public class DamageManager : MonoBehaviour
     [Header("플레이어 공격력")]
     [SerializeField] private float _damage;
     [SerializeField] private float _damageIncrease;
+
+    [Header("공격력 UI")]
+    [SerializeField] private Text _damageTextUI;
+
     public float Damage => _damage;
 
-    private int _socreCost;
+    [Header("파워업 비용")]
+    [SerializeField] private int _socreCost;
+
+    private void Start()
+    {
+        UpdateDamageUI();
+    }
 
     public void PowerUp()
     {
         if (ScoreManager.Instance.CurrentScore < _socreCost) return;
         ScoreManager.Instance.PayScore(_socreCost);
         _damage += _damageIncrease;
+
+        UpdateDamageUI();
+    }
+
+    private void UpdateDamageUI()
+    {
+        _damageTextUI.text = _damage.ToString();
     }
 
 }
